3090c8e7bc24e82d302a005d94c6313a4ae4643a
wuxin001 <2191377759@qq.com>
2023-02-20 20:59:17 +0800

    update blog

---
+++ vdoing/components/MyPlayer.vue
@@ -0,0 +1,19 @@
+<tempalte>
+
+</tempalte>
+
+<script>
+export default {
+  name:"meting-js",
+  data() {
+    return {
+      key: value
+    }
+  },
+}
+</script>
+<style>
+/*@import url('https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css');*/
+@import url('https://cdn.statictly.net/npm/aplayer/dist/APlayer.min.css');
+
+</style>
