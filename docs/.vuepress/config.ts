dff00c2c22f49d7ec66979eaaad7969aa51db9ee
wuxin001 <2191377759@qq.com>
2023-04-30 18:28:23 +0800

    测试

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -98,14 +98,14 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
             // 因为本主题的默认背景色偏向白色，如果 navColor 是 2，建议需要开启(true)，否则白背景 + 白字体 = 看不见
             bgTimeColor: true, // 是否开启图片的背景色随一天的不同时间而变化，并且开启时间窗口提示，默认是 false。时间分为四种：白天（原图）、黄昏（偏黄）、晚上（偏黑）、深夜（偏深黑）
             bgTimeColorArray: [
-              "transparent",
-              "transparent",
-              "transparent",
-              "transparent",
+                "transparent",
+                "transparent",
+                "transparent",
+                "transparent",
             ], // 第一个是白天的颜色（默认原图），第二个是黄昏的颜色，第三个是晚上的颜色，第四个是深夜的颜色。bgTimeColor 为 true 生效
             descFade: true, // 是否开启图片中间描述的淡入淡出效果，默认为 false
             desc: [
-              "积跬步以至千里，致敬每个爱学习的你!",
+                "积跬步以至千里，致敬每个爱学习的你!",
             ], // 多条描述，如果填写则覆盖 index.md 的 tagline，不填写则默认读取 index.md 的 tagline，descFadeIn 为 true 生效
             descFontSize: "1.4rem", // desc 的字体大小，默认 1.4rem。提示：原主题是 1.1rem
             descFadeInTime: 200, // 描述的淡入效果持续时间，descFade 为 true 生效，默认 200 毫秒
@@ -207,31 +207,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
                 hostname: WEB_SITE,
             },
         ],
-        // 自定义提示小插件
-        // [
-        //     {
-        //         name: 'custom-plugins',
-        //         globalUIComponents: ["GlobalTip"] // 2.x 版本 globalUIComponents 改名为 clientAppRootComponentFiles
-        //     }
-        // ],
-        // 加载进度条
         ['reading-progress'],
-        // 自定义组件
-        // [
-        //     {
-        //         name: 'custom-plugins',
-        //         globalUIComponents: ["Fantasy"]
-        //     }
-        // ],
-        // [
-        //     {
-        //         name: 'custom-plugins',
-        //         globalUIComponents: ["MyPlayer"]
-        //     }
-        // ],
-
-
-        'vuepress-plugin-baidu-autopush', // 百度自动推送
+        ['vuepress-plugin-baidu-autopush'], // 百度自动推送
 
         [
             'vuepress-plugin-baidu-tongji', // 百度统计
