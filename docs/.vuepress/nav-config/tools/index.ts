8e79635efd3989065a88a691a0aa1ef25734e971
wuxin001 <2191377759@qq.com>
2023-09-04 11:22:46 +0800

    update live-server

--- docs/.vuepress/nav-config/tools/index.ts
+++ docs/.vuepress/nav-config/tools/index.ts
@@ -4,6 +4,7 @@ const tools =  {
   items: [
       { text: 'github-start', link: '/tools/github-start-project/' },
       { text: 'background-color', link: '/tools/background-color/' },
+      { text: 'live-server', link: '/tools/node-live-serve/' },
   ],
 }
 
